b879a7f954923157d9e89a3bb5b5277308a8701b
tomfaulkenberry <tomfaulkenberry@gmail.com>
2019-08-23 09:54:17 -0500

    add canvas materials for 5316

---
+++ canvas/5316/lab1-1.R
@@ -0,0 +1,14 @@
+# PSYC 5301 - Lab 1.1
+# R assignment
+#
+# Instructions: on line 9, change the value of "studentID" to your own Tarleton ID.
+# On line 10, change the value of "openSesameNum" to the number you got from OpenSesame
+# Then execute the code on lines 12-14.  The answer will be a number that will
+# appear in the console.  Submit that number in Canvas.
+
+studentID = 000111222
+openSesameNum = 9999
+
+set.seed(studentID)
+x = rnorm(n=1, mean=openSesameNum, sd=500)
+round(x, digits=2)
